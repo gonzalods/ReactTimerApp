7754486d67ec42a7ca7a22d6a85bf685b0ad6b52
0003299-xubuntuVB <gonzalo.moros.sales@gmail.com>
2016-12-02 16:52:33 +0100

    Support foundation settings

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,18 @@
 var webpackConfig = require('./webpack.config.js');
 
-delete webpackConfig.externals; // Ñapa se ve que en Ubuntu 16.04 no funciona bien.
+// Ñapa se ve que en Ubuntu 16.04 no funciona bien.
+delete webpackConfig.externals; //Se corrige con *
 
 module.exports = function(config){
   config.set({
     browsers: ['Chrome'],
     singleRun: true,
     frameworks: ['mocha'],
-    files:['app/tests/**/*.test.jsx'],
+    files:[ //Corregido el probleam *
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/foundation-sites/dist/foundation.min.js',
+      'app/tests/**/*.test.jsx'
+    ],
     preprocessors: {
       'app/tests/**/*.test.jsx': ['webpack','sourcemap']
     },
